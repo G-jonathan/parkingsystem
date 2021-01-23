1f8c115b1119b776a0c6b4f3d4923d61ab411df6
jonathan <jonathan.developpeur@gmail.com>
2021-01-23 10:28:36 +0100

    modifications_following_the_code_review

--- src/test/java/com/parkit/parkingsystem/AdvantagesCalculatorTest.java
+++ src/test/java/com/parkit/parkingsystem/AdvantagesCalculatorTest.java
@@ -7,9 +7,7 @@ import com.parkit.parkingsystem.service.AdvantagesCalculator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 
 public class AdvantagesCalculatorTest {
 
