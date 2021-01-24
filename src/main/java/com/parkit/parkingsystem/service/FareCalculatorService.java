2d72dcc2e157c90e98dc2286b27803f067c5a669
jonathan <jonathan.developpeur@gmail.com>
2021-01-24 13:56:45 +0100

    start_integration_of_advantages_calculs_in_FaraCalculatorService

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,6 +1,8 @@
 package com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.model.Ticket;
 import java.text.DecimalFormat;
 import java.time.Duration;
@@ -8,18 +10,21 @@ import java.time.LocalDateTime;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().isBefore(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+    public void calculateFare(Ticket ticket, TicketDAO ticketDAO) {
+        if ((ticket.getOutTime() == null) || (ticket.getOutTime().isBefore(ticket.getInTime()))) {
+            throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
         }
+
+        ParkingType parkingType = ticket.getParkingSpot().getParkingType();
 
         LocalDateTime inHour = ticket.getInTime();
         LocalDateTime outHour = ticket.getOutTime();
+
         Duration duration = Duration.between(inHour, outHour);
         double timeToPayInMinutes = (Math.abs(duration.toMinutes()));
-        double timeToPayInHours = timeToPayInMinutes/60;
+        double timeToPayInHours = timeToPayInMinutes / 60;
         DecimalFormat decimalFormat = new DecimalFormat("####.##");
-        double timeToPayInHoursInDecimalFormat = Double.parseDouble(decimalFormat.format(timeToPayInHours)); //rounded
+        double timeToPayInHoursInDecimalFormat = Double.parseDouble(decimalFormat.format(timeToPayInHours));
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
@@ -34,3 +39,5 @@ public class FareCalculatorService {
         }
     }
 }
+//             advantagesCalculator.CalculateFreeTime(ticket);
+//            advantagesCalculator.CalculateDiscountForRecurringUsers(ticket);
